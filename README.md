22a647e7ecdb7b9154f44258d86a7ec0698e3457
Aral Tasher <araltasher@Arals-MacBook-Pro.local>
2017-11-05 00:16:53 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# ReduxCasts
+
+Completed [code](https://github.com/StephenGrider/ReduxCasts) examples from **Udemy Course** - Modern React with Redux
+
+Each example from the course can be found within this repo. You can either look at the files in a completed state, or check out the changes that were made in a particular section by clicking on one of the links below.
---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAvB5qVJoO-EO_Zor9ZuTAqVXvv7MyRIeQ';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'GoPro' }, (videos) => {
+            // this.setState({ videos:videos });
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
