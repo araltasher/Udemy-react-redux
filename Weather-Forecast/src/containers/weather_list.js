6fbe5691a2a569deec62f939646d6cc95076502e
araltasher <araltasher@gmail.com>
2017-11-15 22:05:49 -0500

    Added google maps instead of printing city name

--- Weather-Forecast/src/containers/weather_list.js
+++ Weather-Forecast/src/containers/weather_list.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
-
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const temperatures = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidity = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
+
 
         return (
             <tr key={cityData.city.id}>
                 <td>
-                    {cityData.city.name}
+                    <GoogleMap lon={lon} lat={lat} />
                 </td>
                 <td><Chart data={temperatures} color="orange" units="K" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
@@ -24,18 +26,18 @@ class WeatherList extends Component {
     render() {
         return (
             <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (K)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
         );
     }
 }
