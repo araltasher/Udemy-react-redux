b1d30ba0066d21b7a60ff9baabfcb0de31887db5
araltasher <araltasher@gmail.com>
2018-01-09 00:28:50 -0500

    add submit button and corrected validation

--- Blog-Posts/src/components/posts_new.js
+++ Blog-Posts/src/components/posts_new.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
 
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title For Post"
                     name="title"
@@ -35,6 +46,8 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -46,9 +59,9 @@ function validate(values) {
     if (!values.title) {
         errors.title = "Enter a title!";
     }
-    if(values.title.length < 3){
-        errors.title = "Enter a title that is at least 3 characters";
-    }
+    // if(values.title.length < 3){
+    //     errors.title = "Enter a title that is at least 3 characters";
+    // }
 
     if (!values.categories) {
         errors.categories = "Enter some categories";
