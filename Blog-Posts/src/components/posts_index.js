51d366264c174c769abcbcb2626475a8dd391942
araltasher <araltasher@gmail.com>
2018-01-09 19:53:26 -0500

    completed the blog-project

--- Blog-Posts/src/components/posts_index.js
+++ Blog-Posts/src/components/posts_index.js
@@ -12,14 +12,17 @@ class PostsIndex extends Component {
   renderPosts() {
     return _.map(this.props.posts, posts => {
       return (
-        <li className="list-group-item" key={post.id}>
-          {post.title}
+        <li className="list-group-item" key={posts.id}>
+          <Link to={`/posts/${posts.id}`}>
+          {posts.title}
+          </Link>
         </li>
       );
     });
   }
 
   render() {
+    console.log(this.props.posts);
     return (
       <div>
         <div className="text-xs-right">
@@ -35,7 +38,7 @@ class PostsIndex extends Component {
 }
 
 function mapStateToProps(state) {
-  return { post: state.posts };
+  return { posts: state.posts };
 }
 
 export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
