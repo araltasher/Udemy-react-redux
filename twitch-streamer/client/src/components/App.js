d6d4aeaa616c8c8e0e10e3fcaed4caea0251038a
Aral Tasher <araltasher@gmail.com>
2019-04-12 15:24:13 -0400

    stream app starter files

---
+++ twitch-streamer/client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>Page 1</div>;
+};
+
+const PageTwo = () => {
+  return <div>Page 2</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
