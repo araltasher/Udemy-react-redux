d8a0babcee685467ffc99fcc6ec05fee453115ab
Aral Tasher <araltasher@gmail.com>
2019-04-21 19:49:00 -0400

    created react-form

--- twitch-streamer/client/src/components/streams/StreamCreate.js
+++ twitch-streamer/client/src/components/streams/StreamCreate.js
@@ -2,12 +2,32 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  render() {
-    console.log(this.props);
+  renderInput({ input, label }) {
     return (
-      <form>
-        <Field name="title" />
-        <Field name="description" />
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
