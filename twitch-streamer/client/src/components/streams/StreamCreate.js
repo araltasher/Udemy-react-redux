553cdc21b77ba5c408df3fcd2997b6b0b7e08474
Aral Tasher <araltasher@gmail.com>
2019-04-22 10:47:00 -0400

    error handling with redux-forms

--- twitch-streamer/client/src/components/streams/StreamCreate.js
+++ twitch-streamer/client/src/components/streams/StreamCreate.js
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const classname = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-      <div className="field">
+      <div className={classname}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -19,7 +31,7 @@ class StreamCreate extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
@@ -33,6 +45,24 @@ class StreamCreate extends Component {
   }
 }
 
+/*****  CLIENT_SIDE FORM VALIDATION *****/
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    //  User did not enter a title
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    //  User did not enter a title
+    errors.description = "You must enter a description";
+  }
+  return errors;
+};
+
+/*****  CLIENT_SIDE FORM VALIDATION *****/
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate
 })(StreamCreate);
